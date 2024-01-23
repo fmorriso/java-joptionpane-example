7cd319ba6013f527f564275cb50460a0f94efb78
Frederick Morrison <frederick.morrison@outlook.com>
2024-01-23 08:43:45 -0700

    reformat code

--- src/GameStatus.java
+++ src/GameStatus.java
@@ -1,5 +1,4 @@
-public class GameStatus
-{
+public class GameStatus {
     private int hoursDriven = 0;
     private double milesDriven = 0;
     private double fuelRemaining = 100;
@@ -7,68 +6,62 @@ public class GameStatus
     /**
      * @return the hours driven
      */
-    public int getHoursDriven()
-    {
+    public int getHoursDriven() {
         return hoursDriven;
     }
 
     /**
      * @param hoursDriven the hours driven to set
      */
-    public void setHoursDriven(int hoursDriven)
-    {
+    public void setHoursDriven(int hoursDriven) {
         this.hoursDriven = hoursDriven;
     }
 
     /**
-     * @return the milesDriven
+     * @return the miles driven
      */
-    public double getMilesDriven()
-    {
+    public double getMilesDriven() {
         return milesDriven;
     }
 
     /**
-     * @param milesDriven the milesDriven to set
+     * @param milesDriven the miles driven to set
      */
-    public void setMilesDriven(double milesDriven)
-    {
+    public void setMilesDriven(double milesDriven) {
         this.milesDriven = milesDriven;
     }
 
     /**
      * @return the fuel remaining
      */
-    public double getFuelRemaining()
-    {
+    public double getFuelRemaining() {
         return fuelRemaining;
     }
 
     /**
      * @param fuelRemaining the fuel remaining to set
      */
-    public void setFuelRemaining(double fuelRemaining)
-    {
+    public void setFuelRemaining(double fuelRemaining) {
         this.fuelRemaining = fuelRemaining;
     }
 
     /**
-     * Updates the hours driven by adding the additional hours to the existing total.
+     * Updates the hours driven by adding the additional hours to the existing
+     * total.
      *
-     * @param additionallHoursDriven - the amount of additional hours driven
+     * @param additionallHours - the amount of additional hours driven
      */
-    public void updateHoursDriven(int additionallHoursDriven)
-    {
-        this.hoursDriven += additionallHoursDriven;
+    public void updateHoursDriven(int additionallHours) {
+        this.hoursDriven += additionallHours;
     }
 
     /**
-     * Updates the miles driven by adding the additional miles to the existing total.
+     * Updates the miles driven by adding the additional miles to the existing
+     * total.
      *
      * @param additionalMilesDriven - the amount of additional miles driven.
      */
-    public void updateMilesDriven(double additionalMilesDriven)
-    {
+    public void updateMilesDriven(double additionalMilesDriven) {
         this.milesDriven += additionalMilesDriven;
     }
 }
